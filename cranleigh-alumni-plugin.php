1bb85341d10bd93ed256678af54877a3884b2acf
Fred Bradley <hello@fredbradley.co.uk>
2017-08-16 13:37:49 +0100

    Initial Plugin

---
+++ cranleigh-alumni-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Cranleigh Alumni Plugin
+Plugin URI: https://www.cranleigh.org
+Description: Cranleigh Alumni Plugin
+Version: 1.0
+Author: fredbradley
+Author URI: https://www.cranleigh.org
+License: GPL2
+*/
+
+namespace FredBradley\CranleighAlumniPlugin;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+$plugin = new Plugin();
+echo $plugin->PLUGIN_PATH;
